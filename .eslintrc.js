2433edb36e784b95f032601a430463220a22fabc
pooya parsa <pooya@pi0.ir>
2020-04-14 14:01:48 +0200

    chore: prepare for development

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "extends": [
+    "@nuxtjs/eslint-config-typescript"
+  ],
+  "rules": {
+    'no-useless-constructor': 0
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function bootstrap () {
+  main().catch(console.error)
+}
+
+async function main () {
+  console.log('Hi!')
+}
